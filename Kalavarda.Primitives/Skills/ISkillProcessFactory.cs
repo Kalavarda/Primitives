564b87a23387a515a095f66e26ea2a994184c56f
kalavarda <kulerda.kalavarda@gmail.com>
2021-09-12 11:34:08 +0300

    5

---
+++ Kalavarda.Primitives/Skills/ISkillProcessFactory.cs
@@ -0,0 +1,9 @@
+﻿using Kalavarda.Primitives.Process;
+
+namespace Kalavarda.Primitives.Skills
+{
+    public interface ISkillProcessFactory
+    {
+        IProcess Create(ISkilled initializer, ISkill skill);
+    }
+}
